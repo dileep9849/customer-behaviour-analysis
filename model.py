060dcf751ef117b04dedac10335b6adcff39e54e
dileep9849 <103744983+dileep9849@users.noreply.github.com>
2022-09-08 15:53:59 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,15 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 import joblib
-import matplotlib.pyplot as plt
-import pandas as pd
 import streamlit as st
-from numpy import outer
-from PIL import Image
 model = joblib.load('customer.pkl')
 
 
